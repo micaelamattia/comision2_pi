14890abde9dd1ededa1d38340018f71e9fc7facb
micaelamattia <tianamica37@gmail.com>
2020-06-16 00:43:55 -0300

    modificacion index usuario

---
+++ index.js
@@ -0,0 +1,1 @@
+let index = " soy index"
--- usuario.js
+++ usuario.js
@@ -1,5 +1,6 @@
 let usuario = {
     nombre: "Micaela",
     EMAIL:"tianamica37@gmail.com",
-    contraseña:12346
+    contraseña:12346,
+    pais: "Argentina"
 }
